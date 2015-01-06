164f8386ca7bcac2e7093e4361e98b7fdabb1d65
Zachary Perlmutter <zrp200@gmail.com>
2015-01-06 07:38:10 -0800

    Update linear_graph_spec.rb

--- spec/lib/linear_graph_spec.rb
+++ spec/lib/linear_graph_spec.rb
@@ -1,10 +1,10 @@
 require "linear_graph"
-RSpec.describe LinearGraph
+RSpec.describe LinearGraph do
 	it {is_expected.to have_borders}
 	describe "#new" do
 		describe "f(x)" do
 			context "slope, y_int, x = 1, 0, 5" do
-				subject {(Quad1.new 1, 0).f(5)}
+				subject {(LinearGraph.new 1, 0).f(5)}
 				it {is_expected.to eq 5}
 			end
 		end		
