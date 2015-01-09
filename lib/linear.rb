63eddc9e3103aced23dea91b139112296e6a5836
Zachary Perlmutter <zrp200@gmail.com>
2015-01-08 18:31:45 -0800

    Update linear.rb

--- lib/linear.rb
+++ lib/linear.rb
@@ -1,6 +1,7 @@
+autoload :BigDecimal, "bigdecimal"
 module Linear
-    autoload :Graph "linear/graph"
-    autoload :Function "linear/function"
-    autoload :Standard "linear/standard"
-    autoload :SlopeIntercept "linear/slope_intercept"
+    autoload	:Graph,				"linear/graph"
+    autoload 	:Function,			"linear/function"
+    autoload 	:Standard,			"linear/standard"
+    autoload 	:SlopeIntercept,	"linear/slope_intercept"
 end
