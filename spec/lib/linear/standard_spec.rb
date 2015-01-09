970fa2c173ffe7b1824a21cf94b477c92866bde7
Zachary Perlmutter <zrp200@gmail.com>
2015-01-09 13:02:14 -0800

    Create standard_spec.rb

---
+++ spec/lib/linear/standard_spec.rb
@@ -0,0 +1,11 @@
+require "linear/standard"
+include Linear
+describe Standard do
+	describe "#new" do
+		subject {Standard.new rand rand rand}
+		it {is_expected.to be_kind_of Function}
+		it {is_expected.to be_instance_of Standard}
+		it {is_expected.to respond_to :execute, :y_intercept, :slope}
+	end
+end
+		
