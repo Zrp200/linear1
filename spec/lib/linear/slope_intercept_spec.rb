1507eccbc36c755232103c287d4f52eed188b041
Zachary Perlmutter <zrp200@gmail.com>
2015-01-09 10:43:06 -0800

    Update and rename function_spec.rb to slope_intercept_spec.rb

--- spec/lib/linear/slope_intercept_spec.rb
+++ spec/lib/linear/slope_intercept_spec.rb
@@ -1,9 +1,9 @@
-require "linear/function"
+require "linear/slope_intercept"
 include Linear
-RSpec.describe Function do
+RSpec.describe SlopeIntercept do
 	describe "new" do
 		context "when 1, 0" do
-			subject {Function.new 1, 0}
+			subject {SlopeIntercept.new 1, 0}
 			it {is_expected.to be_direct_variation}
 		end
 	end
