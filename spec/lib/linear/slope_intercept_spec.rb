cd5f1ac35fde43c77622cc362264126507baa27d
Zachary Perlmutter <zrp200@gmail.com>
2015-01-09 13:04:14 -0800

    Update slope_intercept_spec.rb

--- spec/lib/linear/slope_intercept_spec.rb
+++ spec/lib/linear/slope_intercept_spec.rb
@@ -1,7 +1,10 @@
 require "linear/slope_intercept"
 include Linear
 RSpec.describe SlopeIntercept do
-	describe "new" do
+	describe "#new" do
+		subject {SlopeIntercept.new rand rand}
+		it {is_expected.to be_kind_of Function}
+		it {is_expected.to respond_to :execute, :to_fun, :slope, :y_intercept}
 		context "when 1, 0" do
 			subject {SlopeIntercept.new 1, 0}
 			it {is_expected.to be_direct_variation}
