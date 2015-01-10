58367ea0e20e584075c2b32a933870ce75e294a1
Zachary Perlmutter <zrp200@gmail.com>
2015-01-09 19:08:02 -0800

    Update slope_intercept.rb

--- lib/linear/slope_intercept.rb
+++ lib/linear/slope_intercept.rb
@@ -4,8 +4,9 @@ module Linear
 		def direct_variation?
 			@y_intercept.zero?
 		end
-		def to_fun
-			Function.new @slope, @y_intercept
+		def to_s
+			string = "y = #{@slope}x"
+			string << " + #{@y_intercept}" unless direct_variation?
 		end
 	end
 end
