1db0e4809d14727f5c93ed18b391647c530bf182
Zachary Perlmutter <zrp200@gmail.com>
2015-01-09 19:23:44 -0800

    Update slope_intercept.rb

--- lib/linear/slope_intercept.rb
+++ lib/linear/slope_intercept.rb
@@ -1,19 +1,8 @@
 require "linear/function"
 module Linear
 	class SlopeIntercept < Function
-		def direct_variation?
-			@y_intercept.zero?
-		end
 		def to_s
-			string = "y = "
-			string << case @slope
-				when -1 then "-"
-				when 1 then String.new
-			else
-				"#{@slope}"
-			end
-			string << "x"
-			string << " + #{@y_intercept}" unless direct_variation?
+			super.sub /f(x)/, "y"
 		end
 		def to_fun
 			Function.new @slope, @y_intercept
