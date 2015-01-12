b383fed2d1a1e16d385cb901fab55572895e6755
Zachary Perlmutter <zrp200@gmail.com>
2015-01-12 12:28:31 -0800

    Update standard.rb

--- lib/linear/standard.rb
+++ lib/linear/standard.rb
@@ -1,6 +1,6 @@
-require "linear/function"
+require "linear/slope_intercept"
 module Linear
-	class Standard < Function
+	class Standard < SlopeIntercept
 		def initialize(a, b, c)
 			super((c/b)/(c/a), c/b)
 		end
