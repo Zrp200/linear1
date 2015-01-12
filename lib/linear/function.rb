e590ffdb23325c84d3e0a6f6b2758f9d81ca47b6
Zachary Perlmutter <zrp200@gmail.com>
2015-01-12 12:59:00 -0800

    Added ability to put x to a power other than 1

--- lib/linear/function.rb
+++ lib/linear/function.rb
@@ -1,23 +1,22 @@
 module Linear
-	class Function # Is basicly slope-intercept 
-		attr_reader :slope, :y_intercept
-		def initialize(slope, y_intercept)
-			@slope = slope
-			@y_intercept = y_intercept
+	class Function
+		attr_reader :slope, :y_intercept, :power
+		def initialize(slope, y_intercept, power=1)
+			@slope, @y_intercept, @power = slope, y_intercept, power
 		end
 		def execute(x)
 			raise ArgumentError unless x.kind_of? Numeric
-			return @slope * x + @y_intercept
+			return slope * x ** power + y_intercept
 		end
 		def to_s
 			string = "f(x) = "
-			string << case @slope
+			string << case slope
 				when 1 then String.new
 				when -1 then ?-
 			else
-				@slope
+				slope
 			end
-			string << "x"
+			string << ?x
 			string << " + #{@y_intercept}" unless direct_variation?
 			return string
 		end
