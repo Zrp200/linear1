b425c5e06f0159d8e4140bd62807be6770adcc8a
Zachary Perlmutter <zrp200@gmail.com>
2015-01-13 12:18:43 -0800

    Update slope_intercept.rb

--- lib/linear/slope_intercept.rb
+++ lib/linear/slope_intercept.rb
@@ -7,18 +7,6 @@ module Linear
 		def to_function
 			Function.new slope, y_intercept
 		end
-		def direct_variation?
-			y_intercept.zero?
-		end
-		alias dv? direct_variation?
-		def to_direct_variation
-			if direct_variation?
-				require "linear/direct_variation"
-				DirectVariation.new slope
-			else
-				raise "Unable to convert to DirectVariation"
-			end
-		end
 		alias to_dv to_direct_variation
 		def initialize(slope, y_intercept=0)
 			super
