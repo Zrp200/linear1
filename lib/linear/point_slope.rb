1c1385560b578d722f1a10455a53027841201b6a
Zachary Perlmutter <zrp200@gmail.com>
2015-01-14 14:30:38 -0800

    Create point_slope.rb

---
+++ lib/linear/point_slope.rb
@@ -0,0 +1,16 @@
+require "linear/function"
+module Linear
+	class PointSlope < Function
+		attr_reader :x1, :y1
+		def initialize(point, slope)
+			@slope, @x1, @y1 = slope, point[0], point[1]
+		end
+		def y_intercept
+			x1 - y1
+		end
+		def to_slope_intercept
+			require "linear/slope_intercept"
+			SlopeIntercept.new slope, y_intercept
+		end
+		alias to_si to_slope_intercept
+	end
