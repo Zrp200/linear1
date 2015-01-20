931517ca05c3e6d99e3626ebd98d6cfd7ed9a0ea
Zachary Perlmutter <zrp200@gmail.com>
2015-01-20 12:50:51 -0800

    Update standard.rb

--- lib/linear/standard.rb
+++ lib/linear/standard.rb
@@ -3,14 +3,8 @@ module Linear
 	class Standard < Function
 		attr_reader :a, :b, :c
 		def initialize(a, b, c)
-			@a, @b, @c = a.to_i, b.to_i, c.to_i
-		end
-		def y_intercept
-			c / b
-		end
-		def slope
-			c / a
-		end
+			super c / b / a, 1, c / b
+			@a, @b, @c = a.to_f, b.to_f, c.to_f
 		def to_s
 			"#{idx a}x + #{idx b} = #{c}"
 		end
