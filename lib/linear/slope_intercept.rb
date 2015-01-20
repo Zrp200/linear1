179282a31e648e1e9974bb629756b22144367b4c
Zachary Perlmutter <zrp200@gmail.com>
2015-01-20 12:15:55 -0800

    Update slope_intercept.rb

--- lib/linear/slope_intercept.rb
+++ lib/linear/slope_intercept.rb
@@ -1,6 +1,14 @@
 require "linear/function"
 module Linear
 	class SlopeIntercept < Function
+		def self.find(array, index1)
+			i = 0
+			until i == 2
+				raise ArgumentError, "array[#{index1 + i}] must be a number" unless array[index1 + i].to_i == array[index1 + i]
+				i += 1
+			end
+			return new array[index1+0], array[index1+1]
+		end
 		def to_s
 			return super.sub "f(x)", "y"
 		end
