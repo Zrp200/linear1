e7f6f4a346a812bb14220a3f7f3bd515d24d1654
Zachary Perlmutter <zrp200@gmail.com>
2015-01-20 12:53:13 -0800

    Update linear.rb

--- lib/linear.rb
+++ lib/linear.rb
@@ -4,4 +4,5 @@ module Linear
     autoload 	:Standard,			"linear/standard"
     autoload    :SlopeIntercept,	"linear/slope_intercept"
     autoload    :DirectVariation,   "linear/direct_variation"
+    autoload    :PointSlope,        "linear/point_slope"
 end
