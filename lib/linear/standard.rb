fc70d3789ee96f1fd88ba7595637addf4a9e31ff
Zachary Perlmutter <zrp200@gmail.com>
2015-01-21 11:32:26 -0800

    Update standard.rb

--- lib/linear/standard.rb
+++ lib/linear/standard.rb
@@ -1,10 +1,11 @@
-require "linear/slope_intercept"
+require "linear/function"
 module Linear
 	class Standard < Function
 		attr_reader :a, :b, :c
 		def initialize(a, b, c)
 			super c / b / a, 1, c / b
 			@a, @b, @c = a.to_f, b.to_f, c.to_f
+		end
 		def to_s
 			"#{idx a}x + #{idx b} = #{c}"
 		end
