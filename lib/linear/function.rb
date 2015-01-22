61e5201d3d070a29144a70cf8f3c1055990bf534
Zachary Perlmutter <zrp200@gmail.com>
2015-01-22 11:33:10 -0800

    Update function.rb

--- lib/linear/function.rb
+++ lib/linear/function.rb
@@ -1,7 +1,7 @@
 module Linear
 	class Function
 		attr_reader :slope, :y_intercept, :power
-		def initialize(slope, y_intercept=0, power=1)
+		def initialize(slope=1, y_intercept=0, power=1)
 			@slope, @y_intercept, @power = slope, y_intercept, power
 		end
 		def execute x
