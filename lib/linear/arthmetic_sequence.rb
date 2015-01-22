4eb9e70e21ad8a5d781aae712255dda2c3a2eb39
Zachary Perlmutter <zrp200@gmail.com>
2015-01-22 12:20:44 -0800

    Update arthmetic_sequence.rb

--- lib/linear/arthmetic_sequence.rb
+++ lib/linear/arthmetic_sequence.rb
@@ -4,19 +4,14 @@ module Linear
 		alias d common_difference
 		alias to_a a
 		alias slope d
-		def initialize(a)
+		def initialize a
 			i = 0
 			while i < a.length - 2 
 				raise ArgumentError, "Elements must have a common difference" unless a[i+1] - a[i] == a[i+2] - a[i+1]
 				i += 1
 			end
 			@common_difference, @a = a[1] - a[0], a
-		end
-		def y_intercept
-			a.first
-		end
-		def execute(n)
-			a[0] + d * n
+			super @common_difference, @a[0]
 		end
 		def to_s # Modified to count from zero instead of one
 			"a[n] = #{a[0]} + #{idx d}n"
