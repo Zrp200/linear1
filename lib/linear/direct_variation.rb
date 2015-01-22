92677b4c430d076442404fbbbb0bd1ba50b1533a
Zachary Perlmutter <zrp200@gmail.com>
2015-01-22 11:34:16 -0800

    Update direct_variation.rb

--- lib/linear/direct_variation.rb
+++ lib/linear/direct_variation.rb
@@ -1,10 +1,13 @@
 require "linear/slope_intercept"
 module Linear
 	class DirectVariation < SlopeIntercept
-		def initialize(k)
+		def initialize k
 			super k, 0
 		end
 		alias k slope
 		alias constant_of_variation k
+		def self.to_s
+			"y = kx"
+		end
 	end
 end
