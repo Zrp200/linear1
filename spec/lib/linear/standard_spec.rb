ffa8f39243daf41c087b1e603873f4d68ad4fe87
Zachary Perlmutter <zrp200@gmail.com>
2015-01-23 10:42:30 -0800

    Update standard_spec.rb

--- spec/lib/linear/standard_spec.rb
+++ spec/lib/linear/standard_spec.rb
@@ -4,7 +4,7 @@ RSpec.describe Standard do
 	describe "#new" do
 		subject {Standard.new rand, rand, rand}
 		it {is_expected.to be_kind_of Function}
-		it {is_expected.to respond_to :execute, :y_intercept, :slope}
+		it {is_expected.to respond_to :execute, :a, :b, :c, :direct_variation?}
 	end
 end
 		
