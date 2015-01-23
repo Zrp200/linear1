cc424b94cd601a7fd144644010ee9cae5ae1ce1b
Zachary Perlmutter <zrp200@gmail.com>
2015-01-23 10:41:02 -0800

    Update direct_variation_spec.rb

--- spec/lib/linear/direct_variation_spec.rb
+++ spec/lib/linear/direct_variation_spec.rb
@@ -1,3 +1,9 @@
 require "linear/direct_variation"
 include Linear
-RSpec.describe DirectVariation 
+RSpec.describe DirectVariation do
+  describe "#new" do
+    subject {DirectVariation.new rand}
+    it {is_expected.to be_direct_variation}
+    it {is_expected.to be_kind_of SlopeIntercept}
+  end
+end
