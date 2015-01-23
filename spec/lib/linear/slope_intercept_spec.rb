127db4f45d80e80c9cfb8f9c9b0226705a5d722c
Zachary Perlmutter <zrp200@gmail.com>
2015-01-23 12:26:24 -0800

    Update slope_intercept_spec.rb

--- spec/lib/linear/slope_intercept_spec.rb
+++ spec/lib/linear/slope_intercept_spec.rb
@@ -6,6 +6,10 @@ RSpec.describe SlopeIntercept do
 		it {is_expected.to be_kind_of Function}
 		its(:power) {is_expected.to eq 1}
 		it {is_expected.to respond_to :execute, :y_intercept, :to_direct_variation, :to_standard, :slope}
+		describe "given 1" do
+			it "should throw error" do
+				expect{SlopeIntercept.new 1}.to throw_error
+			end
 		context "given 1, 0" do
 			subject {SlopeIntercept.new 3, 6}
 			it {is_expected.to_not be_direct_variation}
