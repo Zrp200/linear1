2cb7eb1fc76df6e6851469513dbf275dc58a3311
Zachary Perlmutter <zrp200@gmail.com>
2015-01-23 10:33:48 -0800

    Added Standard to SlopeIntercept conversion

--- lib/linear/standard.rb
+++ lib/linear/standard.rb
@@ -2,13 +2,21 @@ require "linear/function"
 module Linear
 	class Standard < Function
 		attr_reader :a, :b, :c
-		def initialize(a, b, c)
-			super c / b / a, 1, c / b
+		private :slope, :y_intercept
+		def initialize a, b, c
 			@a, @b, @c = a.to_f, b.to_f, c.to_f
+			super @c / @b / @a, @c / @b
 		end
 		def to_s
 			"#{idx a}x + #{idx b} = #{c}"
 		end
+		def self.to_s
+			"ax + by = c"
+		end
+		def to_slope_intercept
+			SlopeIntercept.new slope, y_intercept
+		end
+		alias to_si to_slope_intercept
 	end
 end
 				
