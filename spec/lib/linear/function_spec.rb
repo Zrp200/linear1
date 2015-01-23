1965458a221cbf6456acf9269a0862ceb677ee38
Zachary Perlmutter <zrp200@gmail.com>
2015-01-22 17:21:46 -0800

    Update function_spec.rb

--- spec/lib/linear/function_spec.rb
+++ spec/lib/linear/function_spec.rb
@@ -3,10 +3,9 @@ RSpec.describe Linear::Function do
   describe "#new" do
     context "given no arguments" do
       subject {Linear::Function.new}
-      its(:power) {is_expected.to eq 1}
-      its(:slope) {is_expected.to eq 1}
+      its(:to_s) {is_expected.to eq "f(x) = x"}
+      its(:power, :slope) {is_expected.to eq 1}
       its(:y_intercept) {is_expected.to be_zero}
-      its(:execute, 0) {is_expected.to be_zero}
     end
   end
 end
