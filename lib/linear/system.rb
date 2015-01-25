0c6796edc8631631f67b3d135d0d55d663999ca3
Zachary Perlmutter <zrp200@gmail.com>
2015-01-25 10:50:51 -0800

    Update system.rb

--- lib/linear/system.rb
+++ lib/linear/system.rb
@@ -11,9 +11,28 @@ module Linear
       @equations.each {|equation| final << equation.execute(x)}
       return final
     end
-    def intersection
-      w = Array.new
-      for equation in equations; end
+    def intersection # @todo
+      require "linear/standard"
+      standard = Array.new
+      for equation in @equations
+        standard << equation.to_standard
+      end
+    end
+    private
+    def gcf(standard_array, attribute) # @param standard_array [Array<Standard>]
+      for equation in standard_array
+        raise ArgumentError, "Equations must be kinds of Standard" unless equation.kind_of? Standard
+      end
+    end
+    def greatest_attr(array, attribute)
+      attr_array = Array.new
+      array.each {|eq| attr_array << eq.public_send(attribute)}
+      return attr_array.sort.last
+    end
+      
+      
+        
+        
   end
 end
     
