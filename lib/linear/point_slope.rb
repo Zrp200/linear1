54955e26ca213d67f09da587fe4488dd7e846105
Zachary Perlmutter <zrp200@gmail.com>
2015-01-26 10:32:30 -0800

    Update point_slope.rb

--- lib/linear/point_slope.rb
+++ lib/linear/point_slope.rb
@@ -2,9 +2,9 @@ require "linear/function"
 module Linear
 	class PointSlope < Function
 		attr_reader :x1, :y1
-		private :slope, :y_intercept
-		def initialize point, slope
-			@slope, @x1, @y1 = slope, point[0], point[1]
+		private :y_intercept
+		def initialize x, y, slope
+			@slope, @x1, @y1 = slope, x, y
 			super @slope, @x1 - @y1
 		end
 		def to_slope_intercept
