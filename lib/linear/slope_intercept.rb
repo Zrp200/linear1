eb8b59bb10e2b6146d2ddf3577b7f8611d63e516
Zachary Perlmutter <zrp200@gmail.com>
2015-01-26 17:19:22 -0800

    Update slope_intercept.rb

--- lib/linear/slope_intercept.rb
+++ lib/linear/slope_intercept.rb
@@ -7,7 +7,7 @@ module Linear
 				raise ArgumentError, "array[#{index1 + i}] must be a number" unless (array[index1 + i].to_i.to_s == array[index1 + i] || array[index1 + i].to_f.to_s == array[index1 + i])
 				i += 1
 			end
-			return new array[index1+0], array[index1+1]
+			return new array[index1+0].to_f, array[index1+1].to_f
 		end
 		def to_s
 			return super.sub "f(x)", "y"
