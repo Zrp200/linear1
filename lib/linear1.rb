ed2a3c3523bd2d436ed8668de8f7f3583875032e
Zachary Perlmutter <zrp200@gmail.com>
2015-01-27 21:46:19 -0800

    Update linear1.rb

--- lib/linear1.rb
+++ lib/linear1.rb
@@ -1,6 +1,7 @@
 require "linear1/system"
 require "linear1/standard"
-require "linear1/slope-intercept"
+require "linear1/slope_intercept"
+require "linear1/point-slope"
 require "linear1/function"
 require "linear1/graph"
 require "linear1/direct_variation"
