c717dc6a07fe97816d169f5e98ddc11f49ee36fb
Zachary Perlmutter <zrp200@gmail.com>
2015-01-27 19:16:20 -0800

    Update and rename linear.rb to linear_algebra.rb

--- lib/linear.rb
+++
@@ -1,9 +0,0 @@
-module Linear
-    autoload    :System,            "linear/system"
-    autoload	:Graph,				"linear/graph"
-    autoload 	:Function,			"linear/function"
-    autoload 	:Standard,			"linear/standard"
-    autoload    :SlopeIntercept,	"linear/slope_intercept"
-    autoload    :DirectVariation,   "linear/direct_variation"
-    autoload    :PointSlope,        "linear/point_slope"
-end
---
+++ lib/linear_algebra.rb
@@ -0,0 +1,9 @@
+module LinearAlgebra
+    autoload    :System,            "linear_algebra/system"
+    autoload	:Graph,				"linear_algebra/graph"
+    autoload 	:Function,			"linear_algebra/function"
+    autoload 	:Standard,			"linear_algebra/standard"
+    autoload    :SlopeIntercept,	"linear_algebra/slope_intercept"
+    autoload    :DirectVariation,   "linear_algebra/direct_variation"
+    autoload    :PointSlope,        "linear_algebra/point_slope"
+end
