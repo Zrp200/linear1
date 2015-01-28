3678ce7cc342eefcc8d4de8442e8756687470448
Zachary Perlmutter <zrp200@gmail.com>
2015-01-27 19:06:14 -0800

    Update graph.rb

--- lib/linear/graph.rb
+++ lib/linear/graph.rb
@@ -34,7 +34,7 @@ module Linear
   				final[y] = Array.new
   				x = 0
   				@@axis[:x].times do
-  					final[y][x] = (to_hash[y] - Graph.origin[:x]) ? to_hash[y] : format_grid(x + Graph.origin[:x], y + Graph.origin[:y])
+  					final[y][x] = (to_hash[y] - @@axis[:x] / 2) ? to_hash[y] : format_grid(x + Graph.origin[:x], y + Graph.origin[:y])
   					x += 1
   				end
   				y += 1
