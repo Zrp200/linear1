e2741013f7145c46dffcaa0df52c003cb468a761
Zachary Perlmutter <zrp200@gmail.com>
2015-01-27 18:49:48 -0800

    Update graph_spec.rb

--- spec/lib/linear/graph_spec.rb
+++ spec/lib/linear/graph_spec.rb
@@ -3,6 +3,7 @@ include Linear
 RSpec.describe Graph do
   describe "#new" do
     context "given Function" do
+      subject {Graph.new Function.new}
       it "should not raise error" do
         expect{Graph.new Function.new}.to_not raise_error
       end
