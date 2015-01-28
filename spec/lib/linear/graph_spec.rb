2d3278a0dae4e4705a43ca0a55399179fd33bd9c
Zachary Perlmutter <zrp200@gmail.com>
2015-01-27 18:45:14 -0800

    Update graph_spec.rb

--- spec/lib/linear/graph_spec.rb
+++ spec/lib/linear/graph_spec.rb
@@ -6,6 +6,7 @@ RSpec.describe Graph do
       it "should not raise error" do
         expect{Graph.new Function.new}.to_not raise_error
       end
+      its(:range & :domain & :to_a) {is_expected.to be_an_instance_of Array}
     end
   end
 end
