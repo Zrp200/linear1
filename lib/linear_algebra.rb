c84420683b777ebf13ee873a99c6fa578ed759a1
Zachary Perlmutter <zrp200@gmail.com>
2015-01-27 20:32:22 -0800

    Update linear_algebra.rb

--- lib/linear_algebra.rb
+++ lib/linear_algebra.rb
@@ -1,4 +1,22 @@
 module LinearAlgebra
+    library = %i[System Graph Function Standard SlopeIntercept DirectVariation PointSlope]
+    paths = Array.new
+    for constant in library
+        path = constant.to_s
+        until path ~= /[[:upper:]]/).nil?
+            index = path ~= /[[:upper:]]/
+            path[index].downcase!
+            path = (path.insert(index, "_") unless index.zero?
+        end
+        autoload constant, "linear1/#{path}"
+    end
+                
+        
+        
+        
+=begin       
+    for class in classes
+        autoload class, "linear1/#{class.to_s"
     autoload    :System,            "linear_algebra/system"
     autoload	:Graph,				"linear_algebra/graph"
     autoload 	:Function,			"linear_algebra/function"
@@ -7,3 +25,4 @@ module LinearAlgebra
     autoload    :DirectVariation,   "linear_algebra/direct_variation"
     autoload    :PointSlope,        "linear_algebra/point_slope"
 end
+=end
