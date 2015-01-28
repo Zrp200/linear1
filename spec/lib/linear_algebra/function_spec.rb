5f2397715ce9605096a04d90388b08b85439f89c
Zachary Perlmutter <zrp200@gmail.com>
2015-01-27 19:31:11 -0800

    Update and rename spec/lib/linear/function_spec.rb to spec/lib/linear_algebra/function_spec.rb

--- spec/lib/linear_algebra/function_spec.rb
+++ spec/lib/linear_algebra/function_spec.rb
@@ -1,8 +1,9 @@
-require "linear/function"
-RSpec.describe Linear::Function do
+require "linear_algebra/function"
+include LinearAlgebra
+RSpec.describe Function do
   describe "#new" do
     context "given no arguments" do
-      subject {Linear::Function.new}
+      subject {Function.new}
       its(:to_s) {is_expected.to eq "f(x) = x"}
       its(:power) {is_expected.to eq 1}
       its(:slope) {is_expected.to eq 1}
