edb420aaff055a9b14aa7a60f16ff42195f5703c
Zachary Perlmutter <zrp200@gmail.com>
2015-01-29 08:21:33 -0800

    Update arthmetic_sequence.rb

--- lib/linear1/arthmetic_sequence.rb
+++ lib/linear1/arthmetic_sequence.rb
@@ -3,6 +3,7 @@ module Linear1
 	class ArthmeticSequence < Function
 		attr_reader :a, :common_difference
 		alias d common_difference
+		private :slope, :y_intercept
 		alias to_a a
 		alias slope d
 		def initialize a
@@ -11,7 +12,7 @@ module Linear1
 				raise ArgumentError, "Elements must have a common difference" unless a[i+1] - a[i] == a[i+2] - a[i+1]
 				i += 1
 			end
-			@common_difference, @a = a[1] - a[0], a
+			@common_difference, @a = display_num(a[1]) - display_num(a[0]), a
 			super @common_difference, @a[0]
 		end
 		def to_s # Modified to count from zero instead of one
