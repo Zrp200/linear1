ab97eb268aa4da21ddb6a71cce56e09c14138685
Zachary Perlmutter <zrp200@gmail.com>
2015-01-28 19:36:01 -0800

    Update point_slope.rb

--- lib/linear1/point_slope.rb
+++ lib/linear1/point_slope.rb
@@ -2,13 +2,14 @@ require "linear1/function"
 module Linear1
 	class PointSlope < Function
 		attr_reader :x1, :y1
-		private :y_intercept
+		private :y_intercept, :power
 		def initialize slope, x, y
 			@slope, @x1, @y1 = slope.to_f, x.to_f, y.to_f
 			super @slope, @x1 - @y1
 		end
 		def self.find index
 			new ARGV[index], ARGV[index + 1], ARGV[index + 2]
+		end
 		def to_slope_intercept
 			require "linear1/slope_intercept"
 			SlopeIntercept.new slope, y_intercept
