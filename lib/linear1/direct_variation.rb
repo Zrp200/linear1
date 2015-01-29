c2df64447551c120a796af5b91d47bf044586dff
Zachary Perlmutter <zrp200@gmail.com>
2015-01-28 19:44:40 -0800

    Update direct_variation.rb

--- lib/linear1/direct_variation.rb
+++ lib/linear1/direct_variation.rb
@@ -2,9 +2,10 @@ require "linear1/slope_intercept"
 module Linear1
 	class DirectVariation < SlopeIntercept
 		# @param index1 [Fixnum] the index of the first argument
-		# @param array [Array<String>] the array to be analyzed
-		# @todo
-		def self.find(index1, array=ARGV); end
+		# @return [DirectVariation]
+		def self.find index1
+			new ARGV[index1]
+		end
 		def initialize k=1
 			super k, 0
 		end
