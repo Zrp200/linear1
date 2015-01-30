30410b7fe69c452f0dfddaba798584261053b8d7
Zachary Perlmutter <zrp200@gmail.com>
2015-01-30 11:58:13 -0800

    Update standard_spec.rb

--- spec/lib/linear1/standard_spec.rb
+++ spec/lib/linear1/standard_spec.rb
@@ -3,10 +3,10 @@ include Linear1
 RSpec.describe Standard do
 	describe "#new" do
 		[	[2, 3, 4], [Rational(5, 4), Complex(3, 4), 6], [2.3, 5.4, 3.6]	].each do |args|
-			describe "given #{spec[0]}, #{spec[1]}, #{spec[2]}" do
-				subject {Standard.new spec[0], spec[1], spec[2]}
+			describe "given #{args[0]}, #{args[1]}, #{args[2]}" do
+				subject {Standard.new args.first, args[1], args[2]}
 				it {is_expected.to be_kind_of Function}
-				its(:to_s) {is_expected.to eq "#{spec[0]}x + #{spec[1]}y = #{spec[2]}"}
+				its(:to_s) {is_expected.to eq "#{args.first}x + #{args[1]}y = #{args[2]}"}
 				it {is_expected.to respond_to :execute, :add, :a, :b, :c, :direct_variation?}
 				
 			end
