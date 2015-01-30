b4c87efc7b8fa9366c50ec0a2846a312e02756eb
Zachary Perlmutter <zrp200@gmail.com>
2015-01-30 10:40:49 -0800

    Added tests

--- spec/lib/linear1/standard_spec.rb
+++ spec/lib/linear1/standard_spec.rb
@@ -2,9 +2,15 @@ require "linear1/standard"
 include Linear1
 RSpec.describe Standard do
 	describe "#new" do
-		subject {Standard.new rand, rand, rand}
-		it {is_expected.to be_kind_of Function}
-		it {is_expected.to respond_to :execute, :a, :b, :c, :direct_variation?}
+		[	[2, 3, 4], [Rational(5, 4), Complex(3, 4), 6], [2.3, 5.4, 3.6]	].each do |args|
+			describe "given #{spec[0]}, #{spec[1]}, #{spec[2]}" do
+				subject {Standard.new spec[0], spec[1], spec[2]}
+				it {is_expected.to be_kind_of Function}
+				its(:to_s) {is_expected.to eq "#{spec[0]}x + #{spec[1]}y = #{spec[2]}"}
+				it {is_expected.to respond_to :execute, :add, :a, :b, :c, :direct_variation?}
+				
+			end
+		end
 	end
 end
 		
