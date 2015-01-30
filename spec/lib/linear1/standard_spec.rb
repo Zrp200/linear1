c5106e5204e6b0d3cd3caf3facb7590af0fcbaca
Zachary Perlmutter <zrp200@gmail.com>
2015-01-30 12:20:21 -0800

    Update standard_spec.rb

--- spec/lib/linear1/standard_spec.rb
+++ spec/lib/linear1/standard_spec.rb
@@ -2,7 +2,7 @@ require "linear1/standard"
 include Linear1
 RSpec.describe Standard do
 	describe "#new" do
-		[	[2, 3, 4], [Rational(5, 4), Rational(3, 7), Rational(9, 11)], [2.3, 5.4, 3.6], [Complex(3, 4), Complex(9, 7), Complex(10, 13)]	].each do |args|
+		[	[5_000_000, 3_969_567, 4_000_456], [2, 3, 4], [Rational(5, 4), Rational(3, 7), Rational(9, 11)], [2.3, 5.4, 3.6], [Complex(3, 4), Complex(9, 7), Complex(10, 13)]	].each do |args|
 			describe "given #{args[0]}, #{args[1]}, #{args[2]}" do
 				subject {Standard.new args.first, args[1], args[2]}
 				it {is_expected.to be_kind_of Function}
