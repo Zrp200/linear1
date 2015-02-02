d4a7e56787d866d838dd648917b9f5a855a64509
Zachary Perlmutter <zrp200@gmail.com>
2015-02-02 10:44:59 -0800

    Update arithmetic_sequence_spec.rb

--- spec/lib/linear1/arithmetic_sequence_spec.rb
+++ spec/lib/linear1/arithmetic_sequence_spec.rb
@@ -1,6 +1,6 @@
 require %w[linear1/arithmetic_sequence]
 include Linear1
-RSpec.describe ArithmeticSequence
+RSpec.describe ArithmeticSequence do
   describe "#new" do
     [[2, 3, 4, 5, 6], [3, 6, 9, 12], [1, 3, 5, 7, 9]].each do |args|
       context "given #{args}" do
