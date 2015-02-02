da6610114f37a86904d1dd8b659e266e1caec9cf
Zachary Perlmutter <zrp200@gmail.com>
2015-02-02 12:54:12 -0800

    Update arithmetic_sequence_spec.rb

--- spec/lib/linear1/arithmetic_sequence_spec.rb
+++ spec/lib/linear1/arithmetic_sequence_spec.rb
@@ -2,9 +2,9 @@ require 'linear1/arithmetic_sequence'
 include Linear1
 RSpec.describe ArithmeticSequence do
   describe "#new" do
-    [[2, 3, 4, 5, 6], [3, 6, 9, 12], [1, 3, 5, 7, 9]].each do |args|
+    [[2, 3, 4, 5, 6], [0.5, 1, 1.5, 2.5], [1_000_000, 1_500_000, 2_000_000], [Complex(1, 2), Complex(2, 2), Complex(3, 2)]].each do |args|
       context "given #{args}" do
-        it "should not raise error" do expect{ArithmeticSequence.new args}.to_not raise_error end
+        it "should not raise error" do expect{ArithmeticSequence.new *args}.to_not raise_error end
       end
     end
   end
