27317bec611097b3ffcdd5e2da34c8b6adc0eb28
Zachary Perlmutter <zrp200@gmail.com>
2015-02-02 10:41:15 -0800

    Added specs for Linear1::ArithmeticSequence

---
+++ spec/lib/linear1/arithmetic_sequence_spec.rb
@@ -0,0 +1,11 @@
+require %w[linear1/arithmetic_sequence]
+include Linear1
+RSpec.describe ArithmeticSequence
+  describe "#new" do
+    [[2, 3, 4, 5, 6], [3, 6, 9, 12], [1, 3, 5, 7, 9]].each do |args|
+      context "given #{args}" do
+        it "should not raise error" do expect{ArithmeticSequence.new args}.to_not raise_error end
+      end
+    end
+  end
+end
