39da987e099476b2a7fa814b9e49e16a6ee64d95
Zachary Perlmutter <zrp200@gmail.com>
2015-02-03 23:21:29 -0800

    Update slope_intercept_spec.rb

--- spec/lib/linear1/slope_intercept_spec.rb
+++ spec/lib/linear1/slope_intercept_spec.rb
@@ -6,26 +6,15 @@ RSpec.describe SlopeIntercept do
 		it {is_expected.to be_kind_of Function}
 		its(:power) {is_expected.to eq 1}
 		it {is_expected.to respond_to :execute, :y_intercept, :to_direct_variation, :to_standard, :slope}
-		context "given 1" do
-			it "should raise error" do
-				expect{SlopeIntercept.new 1}.to raise_error
-			end
-		end
-		context "given 1, 0" do
-			subject {SlopeIntercept.new 3, 6}
-			it {is_expected.to_not be_direct_variation}
-			its(:to_s) {is_expected.to eq "y = 3x + 6"}
-		end
-		[ [3, 5], [-2, -9], [4.7, 5.3], [Rational("2/3"), 3], [Rational("-5/8"), -2], [-5, -3] ].each do |spec|
+		[ [3, 5], [-2, -9], [4.7, 5.3], [Complex(3, 5), Rational(3, 5)], [Rational("2/3"), 3], [Rational("-5/8"), -2], [-5, -3] ].each do |spec|
 			context "given #{spec[0]}, #{spec[1]}" do
-				subject {SlopeIntercept.new spec[0], spec[1]}
+				subject {SlopeIntercept.new *spec}
+				it "should not raise error" do expect{SlopeIntercept.new *spec}.to_not raise_error end
 				its(:to_s) {is_expected.to eq "y = #{spec[0]}x + #{spec[1]}"}
 				it "should be graphable" do
 					require "linear1/graph"
 					expect{Graph.new subject}.to_not raise_error
 				end
-				
-
 			end
 		end
 			
