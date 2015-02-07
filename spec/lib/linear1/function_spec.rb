133a99254db52422604289a08ccf08f85fc7f43d
Zachary Perlmutter <zrp200@gmail.com>
2015-02-06 21:56:24 -0800

    Update function_spec.rb

--- spec/lib/linear1/function_spec.rb
+++ spec/lib/linear1/function_spec.rb
@@ -1,13 +1,10 @@
 require "linear1/function"
 include Linear1
 RSpec.describe Function do
-  describe "#new" do
-    context "given no arguments" do
-      subject {Function.new}
-      its(:to_s) {is_expected.to eq "f(x) = x"}
-      its(:power) {is_expected.to eq 1}
-      its(:slope) {is_expected.to eq 1}
-      its(:y_intercept) {is_expected.to be_zero}
-    end
+  describe Function.new
+    its(:to_s) {is_expected.to eq "f(x) = x"}
+    its(:power) {is_expected.to eq 1}
+    its(:slope) {is_expected.to eq 1}
+    its(:y_intercept) {is_expected.to be_zero}
   end
 end
