24240bda4275b00bfb79ed7b217e6d01526f11b0
Zachary Perlmutter <zrp200@gmail.com>
2015-02-06 21:51:53 -0800

    Update system.rb

--- lib/linear1/system.rb
+++ lib/linear1/system.rb
@@ -1,6 +1,16 @@
 module Linear1
   class System
+    include Enumerable
     attr_reader :e1, :e2
+    def each &block
+      if block_given?
+        block.call @e1
+        block.call @e2
+      else
+        yield @e1
+        yield @e2
+      end
+    end
     def initialize equation1, equation2
       @e1, @e2 = equation1, equation2
     end
