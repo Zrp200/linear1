9a227495d995583a6cfc193f6f797993f24a54f5
Zachary Perlmutter <zrp200@gmail.com>
2015-02-08 10:55:29 -0800

    Update standard.rb

--- lib/linear1/standard.rb
+++ lib/linear1/standard.rb
@@ -3,45 +3,63 @@ autoload :SlopeIntercept, "linear1/slope_intercept"
 autoload :PointSlope, "linear1/point_slope"
 module Linear1
 	class Standard < Function
+		
 		attr_reader :a, :b, :c
 		private :slope, :y_intercept
+		
+		
 		def self.find index
 			array, $equation_index = ARGV[index..(index + 2)], $equation_index = index + 3
 			new array[0], array[1], array[2]
 		end
+		
+		
 		def initialize a, b, c
 			@a, @b, @c = display_num(a), display_num(b), display_num(c)
 			super @c / @b / @a, @c / @b
 		end
-		def *(factor)
+		
+		
+		def multiply(factor)
 			new @a * factor, @b * factor, @c * factor
 		end
-		def *=(factor)
-			self * factor
+		
+		alias mult multiply
+		
+		def multiply!(factor)
+			self = multiply factor
 		end
-		def +=(term)
-			@a += term
-			@b += term
-			@c += term
+		
+		alias mult! multiply!
+		
+		
+		def add term
+			new @a + term, @b + term, @c + term
 		end
-		def +(term); end
-		alias add +
-		alias multiply *
-		def add int
-			new @a + int, @b + int, @c + int
+	
+		def add! term
+			self = add term
 		end
-		def to_s
+		
+	
+		def to_s # @return [String] the equation
 			"#{idx a}x + #{idx b}y = #{c}"
 		end
+		
+		
 		def to_slope_intercept
 			SlopeIntercept.new slope, y_intercept
 		end
+		
+		alias to_si to_slope_intercept
+		
+		
 		def to_point_slope
 			d = rand(100) / 2
 			PointSlope.new @c - d, @c + d, slope
 		end
+		
 		alias to_ps to_point_slope
-		alias to_si to_slope_intercept
 	end
 end
 				
