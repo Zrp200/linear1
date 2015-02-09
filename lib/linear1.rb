b9f7ffbe3648d41ca8f3f30011ff094490dbbff2
Zachary Perlmutter <zrp200@gmail.com>
2015-02-08 22:01:33 -0800

    Update linear1.rb

--- lib/linear1.rb
+++ lib/linear1.rb
@@ -1,8 +1,20 @@
-require "linear1/system"
-require "linear1/standard"
-require "linear1/slope_intercept"
-require "linear1/point_slope"
-require "linear1/function"
-require "linear1/graph"
-require "linear1/direct_variation"
-
+module Linear1
+  
+  autoload :System, "linear1/system"
+  autoload :Standard, "linear1/standard"
+  autoload :SlopeIntercept"linear1/slope_intercept"
+  autoload :PointSlope, "linear1/point_slope"
+  autoload :Function, "linear1/function"
+  autoload :Graph, "linear1/graph"
+  autoload :DirectVariation, "linear1/direct_variation"
+  autoload :ArithmeticSequence, "linear1/arithmetic_sequence"
+  
+  EQUATIONS = {
+    function: Function,
+    slope_intercept: SlopeIntercept,
+    arithmetic_sequence: ArithmeticSequence,
+    direct_variation: DirectVariation,
+    point_slope: PointSlope,
+    standard: Standard
+  }
+end
