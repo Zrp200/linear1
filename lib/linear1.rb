15ed944334bccb9431cdc5323271475201ccf478
Zachary Perlmutter <zrp200@gmail.com>
2015-03-18 10:29:37 -0800

    Update linear1.rb

--- lib/linear1.rb
+++ lib/linear1.rb
@@ -6,14 +6,3 @@ require "linear1/function"
 require "linear1/graph"
 require "linear1/direct_variation"
 require "linear1/arithmetic_sequence"
-
-module Linear1  
-  EQUATIONS = {
-    function: Function,
-    slope_intercept: SlopeIntercept,
-    arithmetic_sequence: ArithmeticSequence,
-    direct_variation: DirectVariation,
-    point_slope: PointSlope,
-    standard: Standard
-  }
-end
