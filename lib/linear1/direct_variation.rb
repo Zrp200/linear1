dabc64bb448955df845c5644b65d0060d1164343
Zachary Perlmutter <zrp200@gmail.com>
2015-03-19 11:28:57 -0800

    Update direct_variation.rb

--- lib/linear1/direct_variation.rb
+++ lib/linear1/direct_variation.rb
@@ -1,26 +1,20 @@
 require "linear1/slope_intercept"
 module Linear1
 	class DirectVariation < SlopeIntercept
-		class << self
-			# @param index1 [Fixnum] the index of the first argument
-			# @return [DirectVariation]
-			def find index1
-				new ARGV[index1]
-			end
-			def build
-				print "Enter the constant of variation: "
-				k = STDIN.gets.chomp
-				puts ?\n
-				new k
-			rescue
-				puts "Please try again"
-				retry
-			end
+		def self.try_convert object # @param object [{#to_dv}]
+			object.send case object
+				when ->(obj) {obj.respond_to? :to_direct_variation} then :to_direct_variation
+				when ->(obj) {obj.respond_to? :to_dv} then :to_dv
+			else fail TypeError, "Could not convert #{object} to Direct Variation" end
 		end
 		def initialize k=1
 			super display_num(k), 0
 		end
 		alias k slope
 		alias constant_of_variation k
+	end
+	module_function; def DirectVariation object
+		if object.is_kind_of? Numeric then DirectVariation.new
+		else DirectVariation.try_convert object end
 	end
 end
