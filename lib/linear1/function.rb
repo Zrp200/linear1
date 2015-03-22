dcb26043d9ed1bc48240c43012beae0d6d5a8099
Zachary Perlmutter <zrp200@gmail.com>
2015-03-21 21:56:54 -0700

    Formatted code

--- lib/linear1/function.rb
+++ lib/linear1/function.rb
@@ -10,8 +10,8 @@ module Linear1
 				case num
 					when 	Complex(num).to_s 	then	Complex(num)
 					when 	Rational(num).to_s 	then 	Rational(num)
-					when 	num.to_i.to_s	 	then 	num.to_i
-					when	num.to_f.to_s	 	then 	num.to_f
+					when 	num.to_i.to_s	then 	num.to_i
+					when	num.to_f.to_s	then 	num.to_f
 					when	BigDecimal.new(num).to_s	then 	BigDecimal.new num
 				end
 			end
@@ -45,11 +45,11 @@ module Linear1
 		# @return [DirectVariation]
 		# @raise [TypeError]
 		def to_direct_variation
-			if direct_variation?
-				DirectVariation.new slope
+			if direct_variation? then DirectVariation.new slope
 			else raise TypeError, "Unable to convert to DirectVariation"
 			end
 		end
+		alias to_dv to_direct_variation
 		# @raise [TypeError]
 		# Implicit converter to SlopeIntercept
 		# @return [SlopeIntercept]
@@ -61,22 +61,22 @@ module Linear1
 		def to_function
 			self
 		end
-		alias to_dv to_direct_variation
+		alias to_func to_function
 		private
 		def power_string
 			final = String.new
 			final += "\u207b" if @power < 0
 			final << case @power
-				when 0 then "\u2070"
-				when 1 then "\u00b9"
-				when 2 then "\u00b2"
-				when 3 then "\u00b3"
-				when 4 then "\u2074"
-				when 5 then "\u2075"
-				when 6 then "\u2076"
-				when 7 then "\u2077"
-				when 8 then "\u2078"
-				when 9 then "\u2079"
+				when	0	then	"\u2070"
+				when	1	then	"\u00b9"
+				when	2	then	"\u00b2"
+				when	3	then	"\u00b3"
+				when	4	then	"\u2074"
+				when	5	then	"\u2075"
+				when	6	then	"\u2076"
+				when	7	then	"\u2077"
+				when	8	then	"\u2078"
+				when	9	then	"\u2079"
 			end
 		end
 		def idx(s)
