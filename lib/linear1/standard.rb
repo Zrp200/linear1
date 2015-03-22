133740a098a9b6fd8c474b4d7c056b4f63bfb7cf
Zachary Perlmutter <zrp200@gmail.com>
2015-03-21 21:53:04 -0700

    Added documentation and formatting
    * Removed:
     * Excess `()`s
     * Unneeded Spaces

--- lib/linear1/standard.rb
+++ lib/linear1/standard.rb
@@ -2,9 +2,8 @@ require "linear1/function"
 autoload :SlopeIntercept, "linear1/slope_intercept"
 autoload :PointSlope, "linear1/point_slope"
 module Linear1
-	class Standard < Function
+	class Standard < Function # Standard form consisting of the formula `Ax + By = C`
 		attr_reader :a, :b, :c
-		
 		def self.try_convert(object) # @param object [#to_standard, #to_sf] object to be converted
 			case object
 				when ->(obj) {obj.respond_to? to_standard} then object.to_standard
@@ -12,35 +11,41 @@ module Linear1
 			else fail TypeError, "Cannot convert #{object} to Standard" end
 			
 		end
-		
 		def initialize a, b, c
 			@a, @b, @c = display_num(a), display_num(b), display_num(c)
 			super @c / @b / @a, @c / @b
 		end
-		
-	
 		def to_s # @return [String] the equation
 			"#{idx a}x + #{idx b}y = #{c}"
 		end
 		
-		
+		# Implicit
+		# @return [SlopeIntercept]
 		def to_slope_intercept
 			SlopeIntercept.new slope, y_intercept
 		end
 		
+		# Explicit
+		# @return [SlopeIntercept]
+		# @see (to_slope_intercept)
 		alias to_si to_slope_intercept
 		
+		# Implicit
+		# @return [PointSlope]
 		def to_point_slope
 			d = rand(100) / 2
 			PointSlope.new @c - d, @c + d, slope
 		end
+		
+		# Explicit
+		# @return [PointSlope]
 		alias to_ps to_point_slope
 	end
 	module_function
 	def Standard(*args) # @see Standard.try_convert, Standard.new
 		case args.length
 			when 1 then Standard.new *args 
-			when 3 then Standard.try_convert(object)
+			when 3 then Standard.try_convert object
 		else fail ArgumentError, "Expected 1 or 3 arguments. Got #{args.length}" end
 	end
 end
