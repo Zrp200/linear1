8b28d229e4022751edb3159004ba2e022d90db7d
Zachary Perlmutter <zrp200@gmail.com>
2015-06-01 11:45:55 -0700

    Removed defuct @equations

--- lib/linear1/system.rb
+++ lib/linear1/system.rb
@@ -3,20 +3,19 @@ module Linear1
     include Enumerable
     attr_reader :e1, :e2
     def each
-      return yield(@e1), yield(@e2)
+      yield(@e1), yield(@e2)
     end
     def initialize equation1, equation2
       @e1, @e2 = equation1, equation2
     end
     def execute x
       final = Array.new
-      @equations.each {|equation| final << equation.execute(x)}
+      [@e1, @e2].each {|equation| final << equation.execute(x)}
       final
     end
     def intersection
-      e = [@e1.to_standard, @e2.to_standard]
-      y, x = e[0].b * (e[1].c - e[1].a), e[1].a * (e[0].c - e[0].b)
-      [x, y]
+      e = @e1.to_standard, @e2.to_standard
+      e[1].a * (e[0].c - e[0].b), e[0].b * (e[1].c - e[1].a)
     end
     alias solution intersection
   end
