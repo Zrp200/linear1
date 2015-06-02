99b02d44d1531843a2712c9ec77f9e36ee7a3ad6
Zachary Perlmutter <zrp200@gmail.com>
2015-06-02 12:53:44 -0700

    Update graph_spec.rb

--- spec/lib/linear1/graph_spec.rb
+++ spec/lib/linear1/graph_spec.rb
@@ -2,7 +2,7 @@ require "linear1/graph"
 include Linear1
 RSpec.describe Graph do
   describe "#new" do
-    [Function.new, SlopeIntercept.new(2, 3), Function(3, Rational(5, 7), 6)].each do |e| 
+    [Function.new, SlopeIntercept.new(2, 3), Function.new(3, Rational(5, 7), 6)].each do |e| 
       context "given #{e}" do
         subject {Graph.new e}
         it "should not raise error" do expect{Graph.new e}.to_not raise_error end
