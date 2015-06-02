1505e22e62f5dde10a80450eb88c85ad1bdf1402
Zachary Perlmutter <zrp200@gmail.com>
2015-06-02 12:47:43 -0700

    Update standard_spec.rb

--- spec/lib/linear1/standard_spec.rb
+++ spec/lib/linear1/standard_spec.rb
@@ -11,7 +11,7 @@ RSpec.describe Standard do
 			describe "given #{args[0]}, #{args[1]}, #{args[2]}" do
 				subject {Standard.new *args}
 				it {is_expected.to be_kind_of Function}
-				its(:to_s) {is_expected.to eq "#{args.first.to_s}x + #{args[1].to_s}y = #{args[2].to_s}"}
+				its(:to_s) {is_expected.to eq args.first.to_s + "x + " + args[1].to_s + "y = " + args[2].to_s}
 				it {is_expected.to respond_to :to_dv, :execute, :a, :b, :c, :direct_variation?}
 				
 			end
